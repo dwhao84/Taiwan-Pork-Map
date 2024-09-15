cc3c9230375d50d86df160fc07761be453004966
Dawei84 <dwhao84@gmail.com>
2024-09-16 03:59:10 +0800

    Show the Pig history in Taiwan.

--- Taiwan-Pork-Map/PigHistoryView.swift
+++ Taiwan-Pork-Map/PigHistoryView.swift
@@ -9,8 +9,30 @@ import SwiftUI
 
 struct PigHistoryView: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        NavigationView {
+            VStack(alignment: .leading) {
+                Text("蘭嶼豬")
+                    .bold()
+                    .font(.title)
+                    .multilineTextAlignment(.leading)
+                    .border(Color.black)
+                    .padding()
+                
+                Image(uiImage: Images.lanyuPig)
+                    .resizable()
+                    .frame(width: 350, height: 250)
+                    .cornerRadius(50)
+                    .border(Color.black)
+            }
+            .navigationTitle("🐷的歷史")
+        }
     }
+}
+
+struct PigData {
+    let type: String
+    let image: UIImage
+    let color: Color
 }
 
 #Preview {
