49a8f11e3fbe36f490dc4623597b5486f6528ed5
Dawei84 <dwhao84@gmail.com>
2024-09-16 05:31:30 +0800

    Adjust the UI for pig history.

--- Taiwan-Pork-Map/PigHistoryView.swift
+++ Taiwan-Pork-Map/PigHistoryView.swift
@@ -8,32 +8,49 @@
 import SwiftUI
 
 struct PigHistoryView: View {
+    
+    @State var inputText: String = """
+    豬是非常重要的家畜，哺乳綱偶蹄目豬科的一屬，雜食類動物。
+    地球上二萬五千年前波斯地區即有豬的繪畫、雕刻出現，
+    人類家豬馴化的歷史紀錄是七千年前中國地區發現的。
+    當前臺灣的商業豬種佔有率以洋豬暫時領先，
+    但以肉質味美為主要訴求的臺灣黑豬新品種也由畜試所研發成功。
+    """
+    
     var body: some View {
         NavigationView {
-            VStack(alignment: .leading) {
-                Text("蘭嶼豬")
-                    .bold()
-                    .font(.title)
-                    .multilineTextAlignment(.leading)
-                    .border(Color.black)
-                    .padding()
+            ScrollView(.vertical, showsIndicators: true) {
                 
-                Image(uiImage: Images.lanyuPig)
-                    .resizable()
-                    .frame(width: 350, height: 250)
-                    .cornerRadius(50)
-                    .border(Color.black)
+                VStack(alignment: .leading) {
+                    
+                    TextField("Comment", text: $inputText, prompt: Text("Please input your comment"), axis: .vertical)
+                        .font(.callout)
+                        .padding(EdgeInsets(top: 15, leading: 15, bottom: 15, trailing: 15))
+                        .background(Color(.systemGray6))
+                        .cornerRadius(5)
+                        .frame(height: 250) // Limit the height for better UX
+                        .padding(.horizontal, 16)
+
+                    Text("蘭嶼豬")
+                        .bold()
+                        .font(.title)
+                        .multilineTextAlignment(.leading)
+                        .padding(.horizontal, 10)
+                    
+                    Image(uiImage: Images.lanyuPig)
+                        .resizable()
+                        .frame(width: 360, height: 200)
+                        .cornerRadius(5)
+                        .padding(16)
+
+                }
+                .navigationTitle("台灣豬歷史 🇹🇼")
+                .navigationBarTitleDisplayMode(.inline)
             }
-            .navigationTitle("🐷的歷史")
         }
     }
 }
 
-struct PigData {
-    let type: String
-    let image: UIImage
-    let color: Color
-}
 
 #Preview {
     PigHistoryView()
