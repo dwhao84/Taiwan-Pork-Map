bf8dd95b40c1ce47b31760aeaee739852dbef918
Dawei84 <dwhao84@gmail.com>
2024-09-16 05:45:18 +0800

    Add more section for list.

--- Taiwan-Pork-Map/SettingView.swift
+++ Taiwan-Pork-Map/SettingView.swift
@@ -15,16 +15,27 @@ struct SettingView: View {
         // Set the title text color for both small and large titles
         appearance.titleTextAttributes = [.foregroundColor: UIColor.letters]
         appearance.largeTitleTextAttributes = [.foregroundColor: UIColor.letters]
-
+        
         // Apply the appearance to both standard and scrollEdge appearances
         UINavigationBar.appearance().standardAppearance = appearance
         UINavigationBar.appearance().scrollEdgeAppearance = appearance
     }
-
+    
     var body: some View {
         NavigationView {
             List {
-                Section(header: Spacer().frame(height: 5)) { // Custom spacer as section header
+                Section(header: Spacer().frame(height: 10)) {
+                    HStack {
+                        Image(systemName: "leaf")
+                            .font(.title)
+                            .foregroundStyle(.greyGreen)
+                        Text("農業部相關資訊")
+                            .frame(height: 40)
+                            .foregroundColor(.primary)
+                    }
+                }
+                
+                Section(header: Text("App相關資訊").font(.subheadline)) { // Custom spacer as section header
                     HStack {
                         Image(systemName: "book.pages")
                             .font(.title)
@@ -32,11 +43,22 @@ struct SettingView: View {
                         Text("免責聲明")
                             .frame(height: 40)
                     }
+                    
                     HStack {
                         Image(systemName: "iphone.sizes")
                             .font(.title)
                             .foregroundStyle(.cyan)
                         Text("Option 2")
+                            .frame(height: 40)
+                    }
+                }
+                
+                Section(header: Text("回報問題").font(.subheadline)) {
+                    HStack {
+                        Image(systemName: "envelope")
+                            .font(.title)
+                            .foregroundStyle(.yellow)
+                        Text("免責聲明")
                             .frame(height: 40)
                     }
                 }
