38847c56438173083238e4ed7420b136257b1883
Dawei84 <dwhao84@gmail.com>
2024-09-16 05:31:53 +0800

    Show my setting in SettingView.

--- Taiwan-Pork-Map/SettingView.swift
+++ Taiwan-Pork-Map/SettingView.swift
@@ -8,10 +8,45 @@
 import SwiftUI
 
 struct SettingView: View {
+    
+    init() {
+        let appearance = UINavigationBarAppearance()
+        
+        // Set the title text color for both small and large titles
+        appearance.titleTextAttributes = [.foregroundColor: UIColor.letters]
+        appearance.largeTitleTextAttributes = [.foregroundColor: UIColor.letters]
+
+        // Apply the appearance to both standard and scrollEdge appearances
+        UINavigationBar.appearance().standardAppearance = appearance
+        UINavigationBar.appearance().scrollEdgeAppearance = appearance
+    }
+
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        NavigationView {
+            List {
+                Section(header: Spacer().frame(height: 5)) { // Custom spacer as section header
+                    HStack {
+                        Image(systemName: "book.pages")
+                            .font(.title)
+                            .foregroundStyle(.red)
+                        Text("免責聲明")
+                            .frame(height: 40)
+                    }
+                    HStack {
+                        Image(systemName: "iphone.sizes")
+                            .font(.title)
+                            .foregroundStyle(.cyan)
+                        Text("Option 2")
+                            .frame(height: 40)
+                    }
+                }
+            }
+            .navigationTitle("設定頁面")
+            .navigationBarTitleDisplayMode(.automatic) // Set this inside the NavigationView
+        }
     }
 }
+
 
 #Preview {
     SettingView()
