34d551caabfcb6e4c5c37affd5a03e03f5628f94
Dawei84 <dwhao84@gmail.com>
2024-09-16 04:00:38 +0800

    Define the view model for pig data.

--- Taiwan-Pork-Map/TaiwanPigViewModel.swift
+++ Taiwan-Pork-Map/TaiwanPigViewModel.swift
@@ -5,4 +5,25 @@
 //  Created by Da-Wei Hao on 2024/9/14.
 //
 
-import Foundation
+import SwiftUI
+
+@MainActor
+class TaiwanPigViewModel: ObservableObject {
+    @Published var pigData: [TaiwanPig] = []
+    
+    func fetchPigData () async {
+        guard let url = URL(string: "https://data.moa.gov.tw/Service/OpenData/TransService.aspx?UnitId=tR9TIFWlvquB&IsTransData=1") else {
+            return
+        }
+        
+        do {
+            let (data, _) = try await URLSession.shared.data(from: url)
+            let pigData = try JSONDecoder().decode([TaiwanPig].self, from: data)
+            self.pigData = pigData
+            print(pigData)
+        } catch {
+            print("Error fetching pigs: \(error)")
+        }
+    }
+    
+}
