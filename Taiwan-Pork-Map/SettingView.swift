3a5baa715584af592c05906e803c1b125c54d5a5
Dawei84 <dwhao84@gmail.com>
2024-09-16 05:30:39 +0800

    Add New color set.

---
+++ Taiwan-Pork-Map/SettingView.swift
@@ -0,0 +1,18 @@
+//
+//  SettingView.swift
+//  Taiwan-Pork-Map
+//
+//  Created by Da-Wei Hao on 2024/9/16.
+//
+
+import SwiftUI
+
+struct SettingView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SettingView()
+}
